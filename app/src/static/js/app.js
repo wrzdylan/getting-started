cbcfdac60e2c3025b726f25d814c3b9a7fdb978d
dylan <d.wrzesniewski@groupe-convergence.com>
2022-05-18 17:48:51 +0200

    add docker-compose

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Ajouter une tâche'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
